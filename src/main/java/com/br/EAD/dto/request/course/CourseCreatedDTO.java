554ff22606d6ec0a29483e073d073c00a98c0dc6
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-30 19:07:13 -0300

    updated CourseCreatedDTO - trade NotBlack in category for NotNULL

--- src/main/java/com/br/EAD/dto/request/course/CourseCreatedDTO.java
+++ src/main/java/com/br/EAD/dto/request/course/CourseCreatedDTO.java
@@ -2,13 +2,14 @@ package com.br.EAD.dto.request.course;
 
 import com.br.EAD.enums.Category;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CourseCreatedDTO(
         @NotBlank(message = "This field cannot be empty")
         String name,
         @NotBlank(message = "This field cannot be empty")
         String description,
-        @NotBlank(message = "This field cannot be empty")
+        @NotNull(message = "This field cannot be empty")
         Category category) {
 
 }
