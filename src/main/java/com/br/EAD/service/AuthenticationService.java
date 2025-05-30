57912075048bfaef60e49532c0faf9fde301cdd1
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-30 16:04:59 -0300

    created AuthenticationService implements UserDetailsService

--- src/main/java/com/br/EAD/service/AuthenticationService.java
+++ src/main/java/com/br/EAD/service/AuthenticationService.java
@@ -1,4 +1,22 @@
 package com.br.EAD.service;
 
-public class AuthenticationService {
+import com.br.EAD.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationService implements UserDetailsService{
+
+    private UserRepository userRepository;
+
+    public AuthenticationService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
+    }
 }
