174a5ff755aa9c596adbb3810679c916d00999ce
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-30 17:37:41 -0300

    create Question

--- src/main/java/com/br/EAD/entity/Question.java
+++ src/main/java/com/br/EAD/entity/Question.java
@@ -1,4 +1,34 @@
 package com.br.EAD.entity;
 
+import com.br.EAD.dto.request.Options;
+import com.br.EAD.enums.Type;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_question")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
 public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String statement;
+
+    private Type type;
+
+    private List<Options> options;
+
+    @ManyToOne
+    @JoinColumn(name = "assessment_id")
+    private Assessment assessment;
+
 }
