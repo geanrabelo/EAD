cbe1107b3c9bc70e9ac99b8fa4e983fab7592a53
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-30 18:15:39 -0300

    updated GlobalExceptionHandler - added tratament exception - CourseTittleAlreadyExistsHandler / CourseNotFoundHandler

--- src/main/java/com/br/EAD/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/br/EAD/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.br.EAD.exceptions;
 
 import com.br.EAD.exceptions.dto.ErrorResponse;
+import com.br.EAD.exceptions.ex.CourseNotFound;
+import com.br.EAD.exceptions.ex.CourseTittleAlreadyExists;
 import com.br.EAD.exceptions.ex.UserEmailAlreadyExists;
 import com.br.EAD.exceptions.ex.UserNotFound;
 import org.springframework.http.HttpStatus;
@@ -23,6 +25,16 @@ public class GlobalExceptionHandler {
         return ErrorResponse.notFound(userNotFound.getMessage());
     }
 
+    @ExceptionHandler(CourseNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse courseNotFoundHandler(CourseNotFound courseNotFound){
+        return ErrorResponse.notFound(courseNotFound.getMessage());
+    }
 
+    @ExceptionHandler(CourseTittleAlreadyExists.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse courseTittleAlreadyExistsHandler(CourseTittleAlreadyExists courseTittleAlreadyExists){
+        return ErrorResponse.conflict(courseTittleAlreadyExists.getMessage());
+    }
 
 }
