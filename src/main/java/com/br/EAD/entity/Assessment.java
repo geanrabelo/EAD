9f608397e7904e80bf0ba448ce6f63357ab95ada
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-30 17:31:07 -0300

    created Assessment

--- src/main/java/com/br/EAD/entity/Assessment.java
+++ src/main/java/com/br/EAD/entity/Assessment.java
@@ -1,4 +1,33 @@
 package com.br.EAD.entity;
 
+import com.br.EAD.enums.Type;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_assessment")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
 public class Assessment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tittle;
+
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
