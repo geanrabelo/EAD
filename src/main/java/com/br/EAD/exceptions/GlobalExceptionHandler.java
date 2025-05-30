2e658cfc3e0a5c075ba993b4d8bca269636fd69f
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-30 14:49:14 -0300

    updated GlobalExceptionHandler - UserEmailAlreadyExists

--- src/main/java/com/br/EAD/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/br/EAD/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,19 @@
 package com.br.EAD.exceptions;
 
+import com.br.EAD.exceptions.dto.ErrorResponse;
+import com.br.EAD.exceptions.ex.UserEmailAlreadyExists;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserEmailAlreadyExists.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse userEmailAlreadyExistsHandler(String message){
+        return ErrorResponse.conflict(message);
+    }
+
 }
