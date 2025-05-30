d2e9f0c0615c005be25ff0c9fd36298d7ad04c6b
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-30 14:48:05 -0300

    updated EnumCode - userEmailAlreadyExists

--- src/main/java/com/br/EAD/enums/EnumCode.java
+++ src/main/java/com/br/EAD/enums/EnumCode.java
@@ -1,4 +1,15 @@
 package com.br.EAD.enums;
 
 public enum EnumCode {
+    USR000("Email already registered in system");
+
+    private String message;
+
+    EnumCode(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
 }
