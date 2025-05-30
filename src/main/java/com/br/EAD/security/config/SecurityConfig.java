de31ac5eaab5946c1a26159c03dba8098c342072
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-30 15:47:18 -0300

    updated SecurityConfig - adde SecurityFilter

--- src/main/java/com/br/EAD/security/config/SecurityConfig.java
+++ src/main/java/com/br/EAD/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.br.EAD.security.config;
 
+import com.br.EAD.security.filter.SecurityFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,9 +10,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private SecurityFilter securityFilter;
+
+    public SecurityConfig(SecurityFilter securityFilter){
+        this.securityFilter = securityFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -21,7 +29,7 @@ public class SecurityConfig {
                 //.authorizeHttpRequests(authorize -> authorize
                         //.requestMatchers().hasRole()
                 //)
-                //.addFilterBefore()
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
