68d050143515eb37edc3ffb343e86419a3de959d
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-30 18:19:32 -0300

    update EnumCode - added - CourseExceptions

--- src/main/java/com/br/EAD/enums/EnumCode.java
+++ src/main/java/com/br/EAD/enums/EnumCode.java
@@ -2,7 +2,9 @@ package com.br.EAD.enums;
 
 public enum EnumCode {
     USR000("Email already registered in system"),
-    USR001("User not found");
+    USR001("User not found"),
+    CRS000("Course already registered in system"),
+    CRS001("Course not found");
 
     private String message;
 
