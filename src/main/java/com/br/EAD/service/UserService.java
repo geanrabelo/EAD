105abb20488bf3b3c2f16b2151066f6da7746526
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-30 14:26:30 -0300

    create UserService

--- src/main/java/com/br/EAD/service/UserService.java
+++ src/main/java/com/br/EAD/service/UserService.java
@@ -1,4 +1,11 @@
 package com.br.EAD.service;
 
+import com.br.EAD.dto.request.AuthenticationDTO;
+import com.br.EAD.dto.request.RegisterDTO;
+
 public interface UserService {
+
+    String login(AuthenticationDTO authenticationDTO);
+
+    String register(RegisterDTO registerDTO);
 }
