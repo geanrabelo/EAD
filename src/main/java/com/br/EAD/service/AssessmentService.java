8937fbf0da5ade53374d7bec82df3873476d04a7
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-01 16:19:19 -0300

    update AssessmentService - trade UUID for LONG

--- src/main/java/com/br/EAD/service/AssessmentService.java
+++ src/main/java/com/br/EAD/service/AssessmentService.java
@@ -1,4 +1,23 @@
 package com.br.EAD.service;
 
+import com.br.EAD.dto.request.assessment.AssessmentCreatedDTO;
+import com.br.EAD.dto.request.assessment.AssessmentUpdateDTO;
+import com.br.EAD.dto.response.assessment.AssessmentDetailsDTO;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface AssessmentService {
+
+    String create(AssessmentCreatedDTO assessmentCreatedDTO);
+
+    List<AssessmentDetailsDTO> findAll();
+
+    AssessmentDetailsDTO findById(Long id);
+
+    AssessmentDetailsDTO update(AssessmentUpdateDTO assessmentUpdateDTO);
+
+    Boolean existsById(Long id);
+
+    void deleteById(Long id);
 }
