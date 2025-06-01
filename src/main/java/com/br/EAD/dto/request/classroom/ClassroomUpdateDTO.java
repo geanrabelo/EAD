deeeee1c821da314acf971b9bdf0991fbb900b34
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-01 14:01:26 -0300

    update ClassroomUpdateDTO - added id and Validation Of attributte

--- src/main/java/com/br/EAD/dto/request/classroom/ClassroomUpdateDTO.java
+++ src/main/java/com/br/EAD/dto/request/classroom/ClassroomUpdateDTO.java
@@ -1,11 +1,17 @@
 package com.br.EAD.dto.request.classroom;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record ClassroomUpdateDTO(@NotBlank(message = "This field cannot be empty")
-                                 String tittle,
-                                 @NotBlank(message = "This field cannot be empty")
-                                 String description,
-                                 @NotBlank(message = "This field cannot be empty")
-                                 String linkVideo) {
+import java.util.UUID;
+
+public record ClassroomUpdateDTO(
+         @NotNull(message = "This field cannot be empty")
+         UUID id,
+         @NotBlank(message = "This field cannot be empty")
+         String tittle,
+         @NotBlank(message = "This field cannot be empty")
+         String description,
+         @NotBlank(message = "This field cannot be empty")
+         String linkVideo) {
 }
