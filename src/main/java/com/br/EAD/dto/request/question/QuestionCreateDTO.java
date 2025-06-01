7c1a29afffc24c3a945ed89949fdcf47d83d8dec
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-01 19:29:07 -0300

    update QuestionCreateDTO - added validation of fields

--- src/main/java/com/br/EAD/dto/request/question/QuestionCreateDTO.java
+++ src/main/java/com/br/EAD/dto/request/question/QuestionCreateDTO.java
@@ -1,11 +1,18 @@
 package com.br.EAD.dto.request.question;
 
 import com.br.EAD.enums.Type;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
-public record QuestionCreateDTO(String statement,
-                                Type type,
-                                List<String> options,
-                                Long assessmentId) {
+public record QuestionCreateDTO(
+        @NotBlank(message = "This field cannot be empty")
+        String statement,
+        @NotNull(message = "This field cannot be empty")
+        Type type,
+        @NotNull(message = "This field cannot be empty")
+        List<String> options,
+        @NotNull(message = "This field cannot be empty")
+        Long assessmentId) {
 }
