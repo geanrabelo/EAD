0a4304293f4159933849cc3c321129aaaed28ea9
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-01 13:37:04 -0300

    create ClassroomCreatedDTO

---
+++ src/main/java/com/br/EAD/dto/request/classroom/ClassroomCreatedDTO.java
@@ -0,0 +1,17 @@
+package com.br.EAD.dto.request.classroom;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record ClassroomCreatedDTO(
+        @NotBlank(message = "This field cannot be empty")
+        String tittle,
+        @NotBlank(message = "This field cannot be empty")
+        String description,
+        @NotBlank(message = "This field cannot be empty")
+        String linkVideo,
+        @NotNull(message = "This field cannot be empty")
+        UUID courseId) {
+}
