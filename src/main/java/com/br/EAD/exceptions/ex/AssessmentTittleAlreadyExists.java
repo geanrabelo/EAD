18043b87ddf2e7643a6e84a67c9e9767556dd8a0
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-01 16:01:16 -0300

    create AssessmentTittleAlreadyExists - exception

--- src/main/java/com/br/EAD/exceptions/ex/AssessmentTittleAlreadyExists.java
+++ src/main/java/com/br/EAD/exceptions/ex/AssessmentTittleAlreadyExists.java
@@ -1,7 +1,7 @@
 package com.br.EAD.exceptions.ex;
 
 public class AssessmentTittleAlreadyExists extends RuntimeException {
-  public AssessmentTittleAlreadyExists(String message) {
-    super(message);
-  }
+    public AssessmentTittleAlreadyExists(String message) {
+        super(message);
+    }
 }
