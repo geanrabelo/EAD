ed549ed3abd80eb338f1ba0da5e07eeb225a4be9
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-01 19:20:51 -0300

    create QuestionSatatementAlreadyExists

--- src/main/java/com/br/EAD/exceptions/ex/QuestionStatamentAlreadyExists.java
+++
@@ -1,7 +0,0 @@
-package com.br.EAD.exceptions.ex;
-
-public class QuestionStatamentAlreadyExists extends RuntimeException {
-  public QuestionStatamentAlreadyExists(String message) {
-    super(message);
-  }
-}
---
+++ src/main/java/com/br/EAD/exceptions/ex/QuestionStatementAlreadyExists.java
@@ -0,0 +1,7 @@
+package com.br.EAD.exceptions.ex;
+
+public class QuestionStatementAlreadyExists extends RuntimeException {
+    public QuestionStatementAlreadyExists(String message) {
+        super(message);
+    }
+}
