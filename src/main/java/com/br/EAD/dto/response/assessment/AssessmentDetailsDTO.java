6aaf770037dd55b56ed9ab7135306c3df0976a00
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-01 16:09:52 -0300

    create AssessmentDetailsDTO

--- src/main/java/com/br/EAD/dto/response/assessment/AssessmentDetailsDTO.java
+++ src/main/java/com/br/EAD/dto/response/assessment/AssessmentDetailsDTO.java
@@ -1,4 +1,22 @@
 package com.br.EAD.dto.response.assessment;
 
-public record AssessmentDetailsDTO() {
+import com.br.EAD.entity.Assessment;
+import com.br.EAD.enums.Type;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record AssessmentDetailsDTO(Long id,
+                                   String tittle,
+                                   Type type,
+                                   UUID courseId,
+                                   LocalDateTime createdAt) {
+    public AssessmentDetailsDTO(Assessment assessment){
+        this(assessment.getId(),
+                assessment.getTittle(),
+                assessment.getType(),
+                assessment.getCourse().getId(),
+                assessment.getCreatedAt()
+        );
+    }
 }
