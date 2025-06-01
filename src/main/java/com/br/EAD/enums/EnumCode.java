d6200c56b7f6ea1ded0e81e669e7a44e056e0978
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-01 13:52:43 -0300

    update EnumCode - added message of Classroom exceptions

--- src/main/java/com/br/EAD/enums/EnumCode.java
+++ src/main/java/com/br/EAD/enums/EnumCode.java
@@ -4,7 +4,9 @@ public enum EnumCode {
     USR000("Email already registered in system"),
     USR001("User not found"),
     CRS000("Course already registered in system"),
-    CRS001("Course not found");
+    CRS001("Course not found"),
+    CLR000("Classroom already registered in system"),
+    CLR001("Classroom not found");
 
     private String message;
 
