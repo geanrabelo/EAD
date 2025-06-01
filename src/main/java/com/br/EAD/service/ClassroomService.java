3f885d4d0349ae9746be4f26d57c21a26fa0be20
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-01 13:43:01 -0300

    create ClassroomService - configured

--- src/main/java/com/br/EAD/service/ClassroomService.java
+++ src/main/java/com/br/EAD/service/ClassroomService.java
@@ -1,4 +1,24 @@
 package com.br.EAD.service;
 
+
+import com.br.EAD.dto.request.classroom.ClassroomCreatedDTO;
+import com.br.EAD.dto.request.classroom.ClassroomUpdateDTO;
+import com.br.EAD.dto.response.classroom.ClassroomDetailsDTO;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface ClassroomService {
+
+    String create(ClassroomCreatedDTO classroomCreatedDTO);
+
+    List<ClassroomDetailsDTO> findAll();
+
+    ClassroomDetailsDTO findById(UUID id);
+
+    ClassroomDetailsDTO update(ClassroomUpdateDTO classroomUpdateDTO);
+
+    Boolean existsById(UUID id);
+
+    void deleteById(UUID id);
 }
