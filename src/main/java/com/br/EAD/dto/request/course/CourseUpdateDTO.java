20e7f5c296e1fb2b13ff6bc88aa8a426be6a4479
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-01 13:28:48 -0300

    update CourseUpdateDTO - added validation for update

--- src/main/java/com/br/EAD/dto/request/course/CourseUpdateDTO.java
+++ src/main/java/com/br/EAD/dto/request/course/CourseUpdateDTO.java
@@ -1,5 +1,6 @@
 package com.br.EAD.dto.request.course;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
@@ -7,7 +8,9 @@ import java.util.UUID;
 public record CourseUpdateDTO(
         @NotNull(message = "This field cannot be empty")
         UUID id,
+        @NotBlank(message = "This field cannot be empty")
         String name,
+        @NotBlank(message = "This field cannot be empty")
         String description,
         Boolean active) {
 }
