4342dabd522f4e75d137291bc27b0dc1ddec3ab0
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-01 19:34:20 -0300

    update QuestionDetailsDTO - added List<String> options

--- src/main/java/com/br/EAD/dto/response/question/QuestionDetailsDTO.java
+++ src/main/java/com/br/EAD/dto/response/question/QuestionDetailsDTO.java
@@ -3,15 +3,18 @@ package com.br.EAD.dto.response.question;
 import com.br.EAD.entity.Question;
 import com.br.EAD.enums.Type;
 
+import java.util.List;
 import java.util.UUID;
 
 public record QuestionDetailsDTO(UUID id,
                                  String statement,
+                                 List<String> options,
                                  Type type,
                                  Long assessmentId) {
     public QuestionDetailsDTO(Question question){
         this(question.getId(),
                 question.getStatement(),
+                question.getOptions(),
                 question.getType(),
                 question.getAssessment().getId()
         );
