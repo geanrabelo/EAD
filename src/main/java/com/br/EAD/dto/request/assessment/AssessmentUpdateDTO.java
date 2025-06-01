59ad4de5aeaeb274d6d251aabc779062a053f90b
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-01 16:09:18 -0300

    create AssessmentUpdateDTO

--- src/main/java/com/br/EAD/dto/request/assessment/AssessmentUpdateDTO.java
+++ src/main/java/com/br/EAD/dto/request/assessment/AssessmentUpdateDTO.java
@@ -1,4 +1,15 @@
 package com.br.EAD.dto.request.assessment;
 
-public record AssessmentUpdateDTO() {
+import com.br.EAD.enums.Type;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record AssessmentUpdateDTO(@NotBlank(message = "This field cannot be empty")
+                                  String tittle,
+                                  @NotBlank(message = "This field cannot be empty")
+                                  Type type,
+                                  @NotNull(message = "This field cannot be empty")
+                                  UUID courseId) {
 }
